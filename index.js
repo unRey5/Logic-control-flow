6f0266bb20964edb75b36550d443eb4aac1de970
Daniel Odey <dannyandcode@gmail.com>
2023-10-14 06:22:30 +0100

    worked on calculator challenge using if statements and switch statement

--- index.js
+++ index.js
@@ -109,3 +109,79 @@ switch (true) {
     default:
         console.log('championnnnnnn');
 }
+
+//Calculator challenge
+
+//Create a function called `calculator` that takes three parameters: `num1`, `num2` and `operator`. The operator can be `+`, `-`, `*` or `/`. The function should return the result of the calculation. If anything other than the four operators is passed in, the function should return an error message.
+
+// **Example:**
+
+// ```
+// calculator(5, 2, '+') // returns 7
+// calculator(5, 2, '-') // returns 3
+// calculator(5, 2, '*') // returns 10
+// calculator(5, 2, '/') // returns 2.5
+// calculator(5, 2, '%') // returns an error message
+
+function calculator(num1, num2, operator) {
+    let result;
+
+    if (operator === '+') {
+        result = num1 + num2;
+        
+    } else if (operator === '-') {
+        result = num1 - num2;
+        
+    } else if (operator === '*') {
+        result = num1 * num2;
+        
+    } else if (operator === '/') {
+            result = num1 / num2;
+        }
+        
+    else {
+        console.log('Enter valid operator');
+    }
+
+    return result;
+
+}
+
+console.log(calculator(5,2,'+'));
+console.log(calculator(5,2,'-'));
+console.log(calculator(5,2,'*'));
+console.log(calculator(5,2,'/'));
+console.log(calculator(5,2,'>'));
+
+// function calculator(num1, num2, operator) {
+//     let result;
+//     switch (true) {
+//         case operator === '+':
+//             result = num1 + num2;
+//             break;
+        
+//         case operator === '-':
+//             result = num1 - num2;
+//             break;
+        
+//         case operator === '*':
+//             result = num1 * num2;
+//             break;
+        
+//         case operator === '/':
+//             result = num1 / num2;
+//             break;
+    
+//         default:
+//             result = 'Error: enter valid operator';
+//             break;
+//     }
+
+//     return result;
+// }
+
+// console.log(calculator(5,2,'+'));
+// console.log(calculator(5,2,'-'));
+// console.log(calculator(5,2,'*'));
+// console.log(calculator(5,2,'/'));
+// console.log(calculator(5,2,'>'));
